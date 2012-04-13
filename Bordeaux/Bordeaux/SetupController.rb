592915991ff9ae20d5c776e25d8bd0ddae8d1dbe
Chris Maddox <tyre77@gmail.com>
2012-04-13 15:57:28 -0400

    SetupController

---
+++ Bordeaux/Bordeaux/SetupController.rb
@@ -0,0 +1,13 @@
+#
+#  SetupController.rb
+#  Bordeaux
+#
+#  Created by Chris Maddox on 4/13/12.
+#  Copyright 2012 __MyCompanyName__. All rights reserved.
+#
+
+
+class SetupController < NSWindowController
+  attr_writer :path_to_root
+  
+end
---
+++ Bordeaux/Bordeaux/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bordeaux
+//
+//  Created by Chris Maddox on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+  return macruby_main("rb_main.rb", argc, argv);
+}
